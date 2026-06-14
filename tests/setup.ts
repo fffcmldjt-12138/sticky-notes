ae914979495798ca094454ba0e007ebf1415c286
FFFCMLDJT-12138 <1756742290@qq.com>
2026-06-15 00:28:07 +0800

    test: stabilize markdown editor geometry

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,5 +2,19 @@ import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
 
-afterEach(cleanup)
+if (typeof Range !== 'undefined') {
+  Range.prototype.getClientRects ??= () => [] as unknown as DOMRectList
+  Range.prototype.getBoundingClientRect ??= () => ({
+    bottom: 0,
+    height: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    width: 0,
+    x: 0,
+    y: 0,
+    toJSON: () => ({})
+  })
+}
 
+afterEach(cleanup)
